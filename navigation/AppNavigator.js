a7d491de35e1a32a67a26dc29c11edb0f4b114e3
HaysS <stanfordhays@gmail.com>
2019-01-09 11:14:44 -0600

    updated appNavigator.js with instructions for updating to react-navigation v3

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -17,3 +17,19 @@ export default createSwitchNavigator(
 		initialRouteName: 'AuthLoading'
 	}
 );
+
+// If you're using react-navigation v3 instead of react-navigation v2, use this code instead:
+/*
+const switchNavigator = createSwitchNavigator(
+	{
+		Main: MainTabNavigator,
+		Auth: AuthStack, 
+		AuthLoading: AuthLoadingScreen,
+	},
+	{
+		initialRouteName: 'AuthLoading'
+	}
+);
+
+export default createAppContainer(switchNavigator)
+*/
