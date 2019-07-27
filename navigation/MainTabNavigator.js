a653831506feb21f9f71150695f91ad8746dd5d3
HaysS <stanfordhays@gmail.com>
2019-07-26 22:59:09 -0500

    init

--- navigation/MainTabNavigator.js
+++ navigation/MainTabNavigator.js
@@ -6,11 +6,18 @@ import TabBarIcon from '../components/TabBarIcon';
 import HomeScreen from '../screens/HomeScreen';
 import LinksScreen from '../screens/LinksScreen';
 import SettingsScreen from '../screens/SettingsScreen';
-import LoginScreen from '../screens/LoginScreen';
 
-const HomeStack = createStackNavigator({
-  Home: HomeScreen,
+const config = Platform.select({
+  web: { headerMode: 'screen' },
+  default: {},
 });
+
+const HomeStack = createStackNavigator(
+  {
+    Home: HomeScreen,
+  },
+  config
+);
 
 HomeStack.navigationOptions = {
   tabBarLabel: 'Home',
@@ -26,36 +33,46 @@ HomeStack.navigationOptions = {
   ),
 };
 
-const LinksStack = createStackNavigator({
-  Links: LinksScreen,
-});
+HomeStack.path = '';
+
+const LinksStack = createStackNavigator(
+  {
+    Links: LinksScreen,
+  },
+  config
+);
 
 LinksStack.navigationOptions = {
   tabBarLabel: 'Links',
   tabBarIcon: ({ focused }) => (
-    <TabBarIcon
-      focused={focused}
-      name={Platform.OS === 'ios' ? `ios-link${focused ? '' : '-outline'}` : 'md-link'}
-    />
+    <TabBarIcon focused={focused} name={Platform.OS === 'ios' ? 'ios-link' : 'md-link'} />
   ),
 };
 
-const SettingsStack = createStackNavigator({
-  Settings: SettingsScreen,
-});
+LinksStack.path = '';
+
+const SettingsStack = createStackNavigator(
+  {
+    Settings: SettingsScreen,
+  },
+  config
+);
 
 SettingsStack.navigationOptions = {
   tabBarLabel: 'Settings',
   tabBarIcon: ({ focused }) => (
-    <TabBarIcon
-      focused={focused}
-      name={Platform.OS === 'ios' ? `ios-options${focused ? '' : '-outline'}` : 'md-options'}
-    />
+    <TabBarIcon focused={focused} name={Platform.OS === 'ios' ? 'ios-options' : 'md-options'} />
   ),
 };
 
-export default createBottomTabNavigator({
+SettingsStack.path = '';
+
+const tabNavigator = createBottomTabNavigator({
   HomeStack,
   LinksStack,
   SettingsStack,
 });
+
+tabNavigator.path = '';
+
+export default tabNavigator;
